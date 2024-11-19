1c93e1723a33d1af465ec58a4b4bfa04b9f0a16d
YagmurPluvia <iyagmurbrdk@gmail.com>
2024-11-19 16:05:52 +0300

    Game-hangman
    A simple implementation of the classic Hangman game built with C# using Visual Studio.

---
+++ Adam Asmaca/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace Adam_Asmaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_kel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label_soylen = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label_ktg = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label_kel
+            // 
+            this.label_kel.AutoSize = true;
+            this.label_kel.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_kel.Location = new System.Drawing.Point(51, 131);
+            this.label_kel.Name = "label_kel";
+            this.label_kel.Size = new System.Drawing.Size(0, 35);
+            this.label_kel.TabIndex = 0;
+            this.label_kel.Tag = "";
+            this.label_kel.Visible = false;
+            this.label_kel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.Location = new System.Drawing.Point(368, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(280, 427);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label_soylen);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label_ktg);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label_kel);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(677, 540);
+            this.panel1.TabIndex = 2;
+            // 
+            // label_soylen
+            // 
+            this.label_soylen.AutoSize = true;
+            this.label_soylen.Location = new System.Drawing.Point(39, 319);
+            this.label_soylen.Name = "label_soylen";
+            this.label_soylen.Size = new System.Drawing.Size(0, 20);
+            this.label_soylen.TabIndex = 6;
+            this.label_soylen.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(113, 249);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 43);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Seç";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 257);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(64, 26);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Visible = false;
+            // 
+            // label_ktg
+            // 
+            this.label_ktg.AutoSize = true;
+            this.label_ktg.Location = new System.Drawing.Point(39, 50);
+            this.label_ktg.Name = "label_ktg";
+            this.label_ktg.Size = new System.Drawing.Size(68, 20);
+            this.label_ktg.TabIndex = 3;
+            this.label_ktg.Text = "Kategori";
+            this.label_ktg.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(43, 351);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(281, 75);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Oyuna Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 561);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_kel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label_ktg;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label_soylen;
+    }
+}
+
---
+++ Adam Asmaca/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Adam_Asmaca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] harfler = { "a", "b", "c", "ç", "d", "e", "f", "g", "ğ", "h", "ı", "i", "j", "k", "l", "m", "n", "o", "ö", "p", "r", "s", "ş", "t", "u", "ü", "v", "y", "z" };
+        adam_Asmaca_Islemleri cl_adam_asmaca;
+        String kelime;
+        String[] seçilen_harfler;
+        int can_hakkı = 7;
+        int kalan_hakkımız;
+        int width, height;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            width = pictureBox1.Width;
+            height = pictureBox1.Height;
+            kalan_hakkımız = can_hakkı;
+            seçilen_harfler = new string[0];
+            cl_adam_asmaca = new adam_Asmaca_Islemleri();
+        }
+        private void kelime_Seç()
+        {
+            adam_Asmaca_Islemleri.yeni_Kelime kelimemiz_ = cl_adam_asmaca.kelime_al();
+            label_ktg.Text = "Bu bir " + kelimemiz_.kategori;
+            kelime = kelimemiz_.kelime;
+            foreach (char harf in kelime.ToCharArray())
+            {
+                label_kel.Text = label_kel.Text + "_ ";
+            }
+            button1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = true;
+            label_soylen.Visible = true;
+            label_kel.Visible = true;
+            label_ktg.Visible = true;
+            button2.Visible = true;
+            kelime_Seç();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string veri = textBox1.Text;
+            bool buldu = false;
+            bool soylendi = false;
+            foreach (var item in seçilen_harfler)
+            {
+                if (veri.ToLower() == item.ToLower())
+                {
+                    soylendi = true;
+                    MessageBox.Show("Bu harfi daha önce söylediniz.");
+                }
+            }
+            if (!soylendi)
+            {
+                for (int i = 0; i < harfler.Length; i++)
+                {
+                    if (veri.ToLower() == harfler[i])
+                    {
+                        buldu = true;
+                        kelimede_bul(veri);
+                        break;
+                    }
+                }
+                if (!buldu)
+                {
+                    MessageBox.Show("29 harften birini giriniz.");
+                }
+                güncelle();
+            }
+            int index = label_kel.Text.IndexOf("_");
+            if (index == -1)
+            {
+                sonraki_bolum();
+            }
+        }
+        private void kelimede_bul(String harf)
+        {
+            bool dogrumu = false;
+            char[] dizi = kelime.ToCharArray();
+            for (int a = 0; a < dizi.Length; a++)
+            {
+                if (dizi[a].ToString() != "")
+                {
+                    if (harf == dizi[a].ToString().ToLower())
+                    {
+                        dogrumu = true;
+                        label_kel.Text = label_kel.Text.Remove(a * 2, 1);
+                        label_kel.Text = label_kel.Text.Insert(a * 2, harf).ToUpper();
+                    }
+                }
+            }
+            if (!dogrumu)
+            {
+                kalan_hakkımız--;
+                pictureBox1.Invalidate();
+            }
+            ekle_harf(harf);
+        }
+        private void güncelle()
+        {
+            label_soylen.Text = "";
+            foreach (string item in seçilen_harfler)
+            {
+                label_soylen.Text = label_soylen.Text + item + " ";
+            }
+        }
+        private void ekle_harf(string harf)
+        {
+            string[] a = new string[seçilen_harfler.Length + 1];
+            for (int i = 0; i < seçilen_harfler.Length; i++)
+            {
+                a[i] = seçilen_harfler[i];
+            }
+            a[a.Length - 1] = harf;
+            seçilen_harfler = a;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen kalem = new Pen(Color.Black, 8);
+            if (kalan_hakkımız < can_hakkı)
+            {
+                e.Graphics.DrawLine(kalem, width / 10, height / 15, width / 10, height / 15 * 14);
+                e.Graphics.DrawLine(kalem, width / 10, height / 15, width / 2, height / 15);
+            }
+            if (kalan_hakkımız < can_hakkı - 1)
+            {
+                e.Graphics.DrawLine(kalem, width / 2, height / 15, width / 2, height / 15 * 3);
+                e.Graphics.DrawEllipse(kalem, width / 2 - width / 10, height / 5, width / 5, height / 10);
+            }
+            if (kalan_hakkımız < can_hakkı - 2)//gövde
+            {
+                e.Graphics.DrawLine(kalem, width / 2, height / 10 * 3, width / 2, height / 10 * 6);
+            }
+            if (kalan_hakkımız < can_hakkı - 3)//sağ kol 
+            {
+                e.Graphics.DrawLine(kalem, width / 2, height / 10 * 3, width / 2 + width / 10, height / 10 * 3 + height / 5);
+            }
+            if (kalan_hakkımız < can_hakkı - 4)//kol
+            {
+                e.Graphics.DrawLine(kalem, width / 2, height / 10 * 3, width / 2 - width / 10, height / 10 * 3 + height / 5);
+            }
+            if (kalan_hakkımız < can_hakkı - 5)//sağ ayak
+            {
+                e.Graphics.DrawLine(kalem, width / 2, height / 10 * 6, width / 2 + width / 10, height / 10 * 6 + height / 10);
+            }
+            if (kalan_hakkımız < can_hakkı - 6)//sol ayak
+            {
+                e.Graphics.DrawLine(kalem, width / 2, height / 10 * 6, width / 2 - width / 10, height / 10 * 6 + height / 10);
+                oyun_bitti();
+            }
+        }
+        private void sonraki_bolum()
+        {
+            temizle();
+            kelime_Seç();
+        }
+        public void oyun_bitti()
+        {
+            MessageBox.Show("Oyun Bitti. Dogru kelime : " + kelime);
+            button1.Visible = true;
+            textBox1.Visible = false;
+            button2.Visible = false;
+            temizle();
+
+        }
+        private void temizle()
+        {
+            seçilen_harfler = new string[0];
+            kalan_hakkımız = can_hakkı;
+            pictureBox1.Invalidate();
+            label_ktg.Text = "";
+            label_kel.Text = "";
+            label_soylen.Text = "";
+            textBox1.Text = "";
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class adam_Asmaca_Islemleri
+    {
+
+        private List<yeni_Kelime> yeni_kelime_listesi;
+        private List<yeni_Kelime> kelime_listesi;
+        public adam_Asmaca_Islemleri()
+        {
+            data_dosyası_al();
+        }
+        public struct yeni_Kelime
+        {
+            public string kelime;
+            public string kategori;
+        }
+        public yeni_Kelime kelime_al()
+        {
+            Random rastg = new Random();
+            int a = rastg.Next(0, kelime_listesi.Count);
+            yeni_Kelime kelimemiz = kelime_listesi[a];
+            return kelimemiz;
+        }
+        private yeni_Kelime yeni_Kelime_Olustur(string kelime, string kategori)
+        {
+            yeni_Kelime kelimemiz = new yeni_Kelime();
+            kelimemiz.kelime = kelime;
+            kelimemiz.kategori = kategori;
+            return kelimemiz;
+        }
+        private void data_dosyası_al()
+        {
+            yeni_kelime_listesi = new List<yeni_Kelime>();
+            StreamReader oku = new StreamReader(@"Data.txt", Encoding.Default);
+            string veri;
+            while ((veri = oku.ReadLine()) != null)
+            {
+                if (veri != "")
+                {
+                    yeni_kelime_listesi.Add(yeni_Kelime_Olustur(veri.Split(',')[0], veri.Split(',')[1]));
+                }
+            }
+            kelime_listesi = yeni_kelime_listesi;
+        }
+    }
+}
